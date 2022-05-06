8a9c33f01399a039fcc196a8fea8818812808809
Peru <pawel@perek.pro>
2022-05-06 16:39:34 +0200

    Remove old imports

--- lib/views/app/components/profile_view.dart
+++ lib/views/app/components/profile_view.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:refugee_help_board_frontend/components/tags.dart';
-import 'package:refugee_help_board_frontend/constants/notice.dart';
-import 'package:refugee_help_board_frontend/schemas/notice/notice_schema.dart';
 import 'package:refugee_help_board_frontend/schemas/user/user_schema.dart';
 import 'package:refugee_help_board_frontend/services/user_service.dart';
 import 'package:refugee_help_board_frontend/stores/user_store.dart';
@@ -69,7 +66,7 @@ Widget profileView(BuildContext ctx, WidgetRef ref) {
 
                         final result = await ref
                             .read(userApiProvider.notifier)
-                            .register(user);
+                            .modify(user);
 
                         ScaffoldMessenger.of(ctx).hideCurrentSnackBar();
 

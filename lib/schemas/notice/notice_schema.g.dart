0ad51e63aa3962ce5868d1043184825878f03419
Magdalena Pieron <62250111+magdapieron@users.noreply.github.com>
2022-06-10 13:57:20 +0200

    Calendar export (#16)
    * Implement calendar functionality
    
    * Implement calendar functionality
    
    * Cleanup print
    
    Co-authored-by: Peru <pawel@perek.pro>

--- lib/schemas/notice/notice_schema.g.dart
+++ lib/schemas/notice/notice_schema.g.dart
@@ -11,6 +11,7 @@ _$_Notice _$$_NoticeFromJson(Map<String, dynamic> json) => _$_Notice(
       type: json['type'] as String,
       description: json['description'] as String,
       tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
+      creationData: json['creationData'] as String?,
     );
 
 Map<String, dynamic> _$$_NoticeToJson(_$_Notice instance) => <String, dynamic>{
@@ -18,4 +19,5 @@ Map<String, dynamic> _$$_NoticeToJson(_$_Notice instance) => <String, dynamic>{
       'type': instance.type,
       'description': instance.description,
       'tags': instance.tags,
+      'creationData': instance.creationData,
     };
